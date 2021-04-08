c0dceec11ff3ed82d419770af0524dcd291ea705
César Rodríguez <cesar.rrguez@gmail.com>
2021-04-08 22:37:22 +0200

    Create IoC implementation

---
+++ src/ExampleDDD.Infrastructure.CrossCutting/IoC.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ExampleDDD.Domain.Common;
+using ExampleDDD.Domain.Interfaces;
+using ExampleDDD.Application.Services;
+using ExampleDDD.Application.Interfaces;
+using ExampleDDD.Infrastructure.Data.Context;
+using ExampleDDD.Infrastructure.Data.Repositories;
+
+namespace ExampleDDD.Infrastructure.CrossCutting
+{
+    public static class IoC
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Application
+            services.AddScoped<IUserService, UserService>();
+
+            // Infrastructure - Data
+            services.AddScoped<IUnitOfWork, DataContext>();
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}
